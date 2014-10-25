514b9743994670e9d245852da0fb9f65711c3342
Yan Qian <qianyan@thoughtworks.com>
2014-10-25 22:30:43 +0800

    refactor test

--- src/test/java/Dom4jTest.java
+++ src/test/java/Dom4jTest.java
@@ -16,14 +16,15 @@ import static org.junit.Assert.assertThat;
 public class Dom4jTest {
     private final SAXReader reader = new SAXReader();
     private Document doc;
+    private Element root;
     @Before
     public void setUp() throws Exception {
         doc = reader.read("src/test/resources/sample.xml");
+        root = doc.getRootElement();
     } 
 
     @Test
     public void should_get_bar_nodes() throws Exception {
-        Element root = doc.getRootElement();
         List nodes = root.selectNodes("//foo/bar");
 
         assertThat(nodes.size() > 0, is(true));
@@ -31,7 +32,6 @@ public class Dom4jTest {
 
     @Test
     public void should_get_the_text_of_bar_element() throws Exception {
-        Element root = doc.getRootElement();
         Node node = root.selectSingleNode("//foo/bar[1]");
         String text = node.getText();
 
@@ -40,7 +40,6 @@ public class Dom4jTest {
 
     @Test
     public void should_modify_a_node_text() throws Exception {
-        Element root = doc.getRootElement();
         Node node = root.selectSingleNode("//foo/bar[1]");
         node.setText("hahaha....");
 
@@ -53,7 +52,6 @@ public class Dom4jTest {
 
     @Test
     public void should_add_par_to_document() throws Exception {
-        Element root = doc.getRootElement();
         Element elem = (Element)root.selectSingleNode("//foo/bar[1]");
         elem.addElement("par")
             .addAttribute("k", "v")
@@ -66,7 +64,6 @@ public class Dom4jTest {
 
     @Test
     public void should_delete_par_from_document() throws Exception {
-        Element root = doc.getRootElement();
         Node elem = root.selectSingleNode("//foo/bar[1]");
         elem.detach();
 
@@ -77,7 +74,6 @@ public class Dom4jTest {
 
     @Test
     public void should_be_able_to_write_to_file() throws Exception {
-        Element root = doc.getRootElement();
         root.addElement("par")
             .addAttribute("k", "v")
             .addText("parparpar....");
